7506b98d3046941e6aff1d7a52b09c65da0b14c7
iancurrie <curriei@mcmaster.ca>
2020-07-14 15:48:19 -0400

    sample test file generator

---
+++ training/address.py
@@ -0,0 +1,75 @@
+import nltk
+GIVEN_LABELS = ('house', 'level', 'unit', 'po_box', 'house_number',
+                'road', 'near',  'city', 'suburb', 'city_district',
+                'state_district', 'state', 'postcode',
+                'country_region', 'country', 'lon', 'lat', 'id', 'hash')
+
+
+class Address:
+    # takes in a list of dictionaries of the form [{'label': ____, 'value': ____},...]
+    def __init__(self, dictionary, order=GIVEN_LABELS):
+        self.address_dict = dictionary
+        self.order = order
+        self.ordered = False
+
+    def __str__(self):
+        if not self.ordered:
+            self.order_address(self.order)
+        accum_string = ''
+        for value in self.address_dict:
+            accum_string += value
+            accum_string += ' '
+        return accum_string.strip()
+
+    #   Description: Sorts csv_dict to create a list of dictionaries such that they are in the same order they would be in an address string written by a human.
+    #   Parameters
+    ##      order:
+    ##      return:     sorts the csv_dict according to label placement
+    def order_address(self, order):
+        address_list = []
+        for i in range(len(order)):
+            counter = 0
+            while counter < len(self.address_dict):
+                if self.address_dict[counter]['label'].lower() == order[i]:
+                    address_list.append(self.address_dict[counter])
+                    break
+                else:
+                    counter += 1
+        self.address_dict = address_list
+
+    def to_conll(self, address):
+        tokens = self._tokenize(address)
+        tags = self._ner_tags(address)
+        pos = self._pos_tags(tokens)
+        conll = ''
+        for i in range(len(tokens)):
+            token_val = tokens[i]
+            pos_val = pos[i][1]
+            tag_val = tags[token_val]
+            conll = conll + '{} {} {} {} \n'.format(token_val, pos_val, pos_val, tag_val)
+        return conll
+
+    def _ner_tags(self, address):
+        tags = {}
+        for part in address:
+            value = part['value'].split(' ')
+            tokens = []
+            tokens = tokens + [word for word in value if word]
+            for i in range(len(tokens)):
+                if i == 0:
+                    tags[tokens[i]] = 'B-' + part['label']
+                else:
+                    tags[tokens[i]] = 'I-' + part['label']
+        return tags
+
+    def _tokenize(self, address):
+        tokens = []
+        for part in address:
+            value = part['value'].split(' ')
+            tokens = tokens + [word for word in value if word]
+        return tokens
+
+    ###ADD POS tagging formula here ###
+    def _pos_tags(self, tokens):
+        tagged = nltk.pos_tag(tokens)
+        return tagged
---
+++ training/generate_sample_test_data.py
@@ -0,0 +1,103 @@
+import csv
+import os
+from training.address import Address
+import pyspark.sql as spark
+
+ROOT_FOLDER_NAME = '../../structured_data/testdata'
+OUT_FILE_NAME = '../data/sample_test_data.csv'
+OA_TO_LIBPOSTAL = {'LAT': '',
+                   'LON': '',
+                   'NUMBER': 'house_number',
+                   'STREET': 'road',
+                   'UNIT': 'unit',
+                   'CITY': 'city',
+                   'DISTRICT': '',
+                   'REGION': 'state',
+                   'POSTCODE': 'postcode',
+                   'ID': '',
+                   'HASH': ''}
+# ///////////////////////////////////////////////////////////////////////////////////////
+#   STEP 1
+#   Owner: Archi & Ian
+#   description: takes root data location and processes data into dictionary
+#
+#   Parameters
+#      FILE_NAME:  root file location
+#      return:     returns list of dictionaries with keys matching opencage
+#      eg output:  [{'houseNumber': '3a', 'road': 'Main St.', 'neighborhood': '', 'city': 'Toronto', 'county':...]
+
+def parse_dir(root_location, country, delimiter=','):
+    dir_contents = next(os.walk(root_location))
+    sub_directories = dir_contents[1]
+
+    label = 'country'
+    parents = [{'label': label, 'value': country}]
+
+    all_addresses = __parse_dir(root_location,delimiter,0,parents)
+    return all_addresses
+
+def __parse_dir(root_location, delimiter, level, parent_info):
+    # really gross recursive function that goes through all files and subfolders
+    # then processes each csv into the list of dictionaries
+
+    dir_contents = next(os.walk(root_location))
+    sub_directories = dir_contents[1]
+    files = dir_contents[2]
+
+    #TODO: Fix Level_labels to reflect the actual relevant label
+    level_labels = ['state', 'city']
+
+    # RECURSIVE STATE: parse through all sub-directories recursively
+    all_addresses = []
+    for sub_dir in sub_directories:
+        folder_name = root_location + '/' + sub_dir
+        label = level_labels[level]
+        parents = parent_info +[{'label': label, 'value': sub_dir}]
+        folder_addresses = __parse_dir(folder_name, delimiter=delimiter, level=level+1,parent_info=parents)
+        all_addresses += folder_addresses
+
+    # BASE CASE: parse through all files in directory
+    for file in files:
+        if len(file) > 3 and file[-4:] == '.csv':
+            file_location = root_location + '/' + file
+            label = level_labels[level]
+            parents = parent_info + get_info_from_file_name(label,file)
+            file_addresses = read_csv(file_location, delimiter, parent_info=parents)
+            all_addresses += file_addresses
+    return all_addresses
+
+def get_info_from_file_name(label, file_name):
+    # Takes in a .csv filename and pulls out the relevent inormation for the address if there is any
+    clean_file_name = file_name.replace('.csv','').replace('city_of_','').replace('municipality_of_','')
+    if clean_file_name == 'countrywide' or clean_file_name == 'statewide':
+        return []
+    return [{'label': label, 'value': clean_file_name}]
+
+def read_csv(file_location, delimiter, parent_info=[]):
+    # Opens a .csv file at location file_location and adds converts each line into a list of dictionaries
+    out_list = []
+    with open(file_location, newline='') as file:
+        reader = csv.reader(file, delimiter=delimiter)
+        headers = next(reader)
+        for row in reader:
+            line = []
+            for i in range(len(row)):
+                label = OA_TO_LIBPOSTAL[headers[i]]
+                value = row[i]
+                if value and label:
+                    line.append({'label': label, 'value': value})
+            for parent in parent_info:
+                line.append(parent)
+            out_list.append(Address(line))
+    return out_list
+
+
+def write_csv(file_name, addresses):
+    with open(file_name, 'w') as file:
+        [file.write(str(address) + '\n') for address in addresses]
+
+def to_spark():
+    spark.read.format('csv').options(header='true').load('../data/sample_test_data.csv')
+
+def main():
+    csv_lists = parse_dir(ROOT_FOLDER_NAME,)
